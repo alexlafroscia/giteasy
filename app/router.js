e6b78a96a78cd68fff5ff2a0db60b49f41412fbb
vonbearshark <jjnaughton93@gmail.com>
2015-02-07 05:09:37 -0500

    some more basic pages stuff

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-this.route("index", {path: "/"});
+  this.route("index", {path: "/"});
+  this.route("repoDir", {path: "/repo-dir"});
+  this.route("index-chooseRepo", {path: "/chooseRepo"});
 });
 
 export default Router;
