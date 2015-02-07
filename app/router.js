b26ee5206fb0e3f8184ca1573bf0daab7c3cb0f6
Alex LaFroscia <alex@lafroscia.com>
2015-02-07 10:04:34 -0500

    Add auth stuff

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route("login");
+  this.route("oauth");
+  this.route("user");
+  this.route("orgs");
 });
 
 export default Router;
---
+++ tests/unit/routes/orgs-test.js
@@ -0,0 +1,14 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+moduleFor('route:orgs', 'OrgsRoute', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function() {
+  var route = this.subject();
+  ok(route);
+});
