f69a1d4ff2c657322b19e75a85367ad5f26c42a0
vonbearshark <jjnaughton93@gmail.com>
2015-02-07 10:26:26 -0500

    fixing merge conflicts

--- app/router.js
+++ app/router.js
@@ -10,16 +10,12 @@ Router.map(function() {
   this.route("oauth");
   this.route("repoDir", {path: "/repo-dir"});
   this.route("index-chooseRepo", {path: "/chooseRepo"});
-<<<<<<< HEAD
-
   this.route("repo-dir", function() {
     this.route("editFile");
   });
-=======
   this.route("login");
   this.route("user");
   this.route("orgs");
->>>>>>> c65918f23c0c993bbc4dc95751dc95beed6b9930
 });
 
 export default Router;
