a8f1e61e0c7558bdb52dde4949da8222509f60f4
vonbearshark <jjnaughton93@gmail.com>
2015-02-07 14:43:38 -0500

    file upload

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -1,14 +1,27 @@
 import Ember from 'ember';
 import FileField from 'ember-uploader/file-field';
 
-export default FileField.extend({
-	filesDidChange: (function() {
+export default FileField.extend(
+{
+	filesDidChange: (function()
+	{
 		var files = this.get('files');
 
-		if (!Ember.isEmpty(files)){
-			//alert("Ready to commit "files[0].name);
-			var text = new FileReader(files[0]).readAsText();
-			alert(text)
+		if (!Ember.isEmpty(files))
+		{
+			var reader = new FileReader();
+
+			repo.write('master', 'path/to/file', files[0].getAsBinary(), 'YOUR_COMMIT_MESSAGE', function(err) {});
+			/*
+			reader.onload = function(e)
+			{
+				var text = reader.result;
+				alert(text);
+				//repo.write('master', 'path/to/file', 'YOUR_NEW_CONTENTS', 'YOUR_COMMIT_MESSAGE', function(err) {});
+			};
+
+			reader.getAsBinary(files[0]);
+			*/	
 		}
 
 
