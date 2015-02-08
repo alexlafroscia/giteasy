449b94bcae365b7d459c1c6437916902e2f5aa11
Alex LaFroscia <alex@lafroscia.com>
2015-02-07 21:16:49 -0500

    Fix tests

--- tests/unit/routes/choose-test.js
+++ tests/unit/routes/choose-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('route:orgs', 'OrgsRoute', {
+moduleFor('route:choose', 'ChooseRoute', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
--- tests/unit/routes/repo-test.js
+++ tests/unit/routes/repo-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('route:repo-dir', 'RepoDirRoute', {
+moduleFor('route:repo', 'RepoRoute', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
