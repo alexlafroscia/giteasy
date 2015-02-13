616b1d40d0fd21e0e386e9a295e39ee473d74cf3
Alex LaFroscia <alex@lafroscia.com>
2015-02-13 22:25:36 +0000

    Rebuild pages at 39512d4

--- assets/giteasy.js
+++ assets/giteasy.js
@@ -2817,7 +2817,7 @@ catch(err) {
 if (runningTests) {
   require("giteasy/tests/test-helper");
 } else {
-  require("giteasy/app")["default"].create({"name":"giteasy","version":"0.0.0.f5201ab0"});
+  require("giteasy/app")["default"].create({"name":"giteasy","version":"0.0.0.39512d47"});
 }
 
 /* jshint ignore:end */
