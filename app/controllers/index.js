43da9146532b0fddccefea21e59ee6eadfa80ee8
Alex LaFroscia <alex@lafroscia.com>
2015-03-16 18:09:42 -0400

    Don't redirect unless it's necessary

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,12 +3,18 @@ import config from '../config/environment';
 
 export default Ember.Controller.extend({
 
+  session: Ember.inject.service('github'),
+
   actions: {
     login: function() {
-      var url = 'https://github.com/login/oauth/authorize?client_id=';
-      url += config.API.GITHUB_CLIENT_ID;
-      url += '&scope=user,repo';
-      location.href = url;
+      if (Ember.isPresent(this.get('session.accessToken'))) {
+        this.transitionToRoute('choose');
+      } else {
+        var url = 'https://github.com/login/oauth/authorize?client_id=';
+        url += config.API.GITHUB_CLIENT_ID;
+        url += '&scope=user,repo';
+        location.href = url;
+      }
     }
   }
 });
