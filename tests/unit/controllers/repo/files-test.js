9bfefde7e130e91e59f261b4d634e17f3c524161
Alex LaFroscia <alex@lafroscia.com>
2015-04-07 15:14:44 -0400

    Add tests for repo/files, repo/index controllers

--- tests/unit/controllers/repo/files-test.js
+++ tests/unit/controllers/repo/files-test.js
@@ -8,8 +8,15 @@ moduleFor('controller:repo/files', 'Controller: repo/files', {
   needs: ['controller:repo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var controller = this.subject();
-  assert.ok(controller);
+test('it properly aliases properties from the repo controller', function(assert) {
+  const repo = {name: 'giteasy'};
+  const path = '/';
+
+  var controller = this.subject({
+    content: path
+  });
+  controller.set('controllers.repo.content', repo);
+
+  assert.deepEqual(controller.get('repo'), repo, 'repo alias is correct');
+  assert.deepEqual(controller.get('path'), path, 'path alias is correct');
 });
