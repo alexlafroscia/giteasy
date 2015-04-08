21c60055a5f611d88ab33951e1e7884472e2fa0a
Alex LaFroscia <alex@lafroscia.com>
2015-04-07 23:15:13 -0400

    Finish repo acceptance tests
    Doesn't cover all functionality, but does cover all currently testable
    functionality

--- tests/acceptance/repo-test.js
+++ tests/acceptance/repo-test.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import startApp from 'giteasy/tests/helpers/start-app';
 
 // Testing
@@ -23,6 +23,11 @@ module('Acceptance: Repo', {
       textStatus: 'success'
     });
 
+    defineFixture('https://api.github.com/repos/alexlafroscia/giteasy/contents/lib/octokit', {
+      response: FileFixtures[0],
+      textStatus: 'success'
+    });
+
     application = startApp();
   },
 
@@ -41,4 +46,45 @@ test('Visiting page shows the title and list of files', function(assert) {
     assert.equal(find('.repo-description').text(), 'This is the description', 'Page has correct description');
     assert.equal(find('ul.card-list li').size(), 2, 'File list has correct amount of contents');
   });
+});
+
+test('Clicking on a directory displays the list of the containing files', function(assert) {
+  assert.expect(1);
+
+  visit('/alexlafroscia/giteasy');
+
+  click('ul.card-list li:eq(0) a');
+  andThen(function() {
+    assert.equal(currentRouteName(), 'repo.files', 'Navigated to the correct route');
+  });
+});
+
+
+/**
+ * The following three things are items that I would like to add tests for
+ * eventually, but the file upload code itself isn't done yet, and I'm not sure
+ * if it's event possible to test "drag-and-drop"-ing a file from the desktop
+ * through QUnit.  There's a chance that I could write a custom test helper to
+ * do this, but I think it would end up being a very complicated process and
+ * possibly not worth doing at this stage of the application.
+ */
+skip('Clicking on a file downloads the file to the local filesystem', function(assert) {
+  assert.expect(1);
+
+  visit('/alexlafroscia/giteasy');
+
+  click('ul.card-list li:eq(1) a');
+  andThen(function() {
+    // Do something in here to verify that the file download was started
+    // Maybe look at making a utility method with different testing behavior,
+    // like the URL changing utility?
+  });
+});
+
+skip('Dragging a file onto the screen adds it to the files to be uploaded', function(assert) {
+  // Is this even possible to test through QUnit??
+});
+
+skip('Clicking the `upload` button initiates an upload of the staged files', function(assert) {
+  // If the previous test hasn't been implemented, this can't be either
 });
