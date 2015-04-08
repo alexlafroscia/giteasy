b07e2a5a1265368a8d2bf42ccfe524c9d514a1d9
Alex LaFroscia <alex@lafroscia.com>
2015-04-08 01:20:43 -0400

    Hopefully add Blob support in Phantom 1.9

--- tests/unit/utils/file-reader-test.js
+++ tests/unit/utils/file-reader-test.js
@@ -6,7 +6,21 @@ module('fileReader', 'Utility: file-reader');
 test('it reads the contents of a file', function(assert) {
   assert.expect(1);
   const filedata = 'this is my file name';
-  const blob = new Blob([filedata], { type: 'text/html'});
+  var blob;
+  try {
+    blob = new Blob([filedata], { type: 'text/html'});
+  } catch(e) {
+    // TypeError old chrome and FF
+    window.BlobBuilder = window.BlobBuilder ||
+                         window.WebKitBlobBuilder ||
+                         window.MozBlobBuilder ||
+                         window.MSBlobBuilder;
+    if(e.name === 'TypeError' && window.BlobBuilder) {
+      var bb = new BlobBuilder();
+      bb.append([filedata.buffer]);
+      blob = bb.getBlob('text/html');
+    }
+  }
   return fileReader(blob).then(function(data) {
     assert.equal(data, filedata, 'The resolved contents match the file contents');
   });
